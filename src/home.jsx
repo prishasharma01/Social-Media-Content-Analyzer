bd4d4b6f4d123797efe156aa76c3c47baae80929
Prisha Sharma <156198799+prishasharma01@users.noreply.github.com>
2025-08-31 19:33:03 +0530

    home.jsx

--- src/home.jsx
+++ src/home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Home.css"; // custom styles
+import "./home.css"; // custom styles
 
 export default function Home() {
   return (
